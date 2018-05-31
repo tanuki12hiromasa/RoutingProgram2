5d1102639a625ea2039779b20e27bbe653732fd1
Unknown <tanuki13.snow@gmail.com>
2018-06-01 00:17:41 +0900

    for report
    見やすいように色々整理

--- SAalgo/Program.cs
+++ SAalgo/Program.cs
@@ -4,13 +4,13 @@ namespace Routing2
 {
     class Program
     {
-        const int width = 9;
-        const int height = 9;
-        const string mapfile = "neighbor_mat3.csv";
-        const string destfile = "destination3.csv";
-        static void Main(string[] args)
+        const int width = 9; //地図の横幅
+        const int height = 9; //地図の縦幅
+        const string mapfile = "..\\..\\..\\..\\neighbor_mat.csv"; //読み込む地図のデータ.
+        const string destfile = "..\\..\\..\\..\\destination.csv";  //読み込む目的地のデータ.
+        static void Main(string[] args)//第一引数は出力ディレクトリ名 第二引数は乱数のシード値
         {
-            bool finalReturn = false;
+            bool finalReturn = true; //最後に帰着するか否か
             Console.WriteLine(System.IO.Directory.GetCurrentDirectory());
             var pf = new PathFinder(int.Parse(args[1]), width, height, finalReturn, args[0]);
             pf.ex(mapfile, destfile);
